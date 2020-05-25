2ca2ca704503a955a8312583611e83c784d27d46
Goose <sv_gulchak@inbox.ru>
2020-05-25 14:41:13 +0300

    Init

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- Coding: UTF-8 -*-
+
+import os, sys
+from PySide2.QtWidgets import QApplication, QWidget
+from widget import *
+
+
+def main(argv):
+    app = QApplication(argv)
+
+    widget = Widget()
+    widget.show()
+
+    return app.exec_()
+
+if '__main__' == __name__:
+    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = './platforms'
+    exit_status = main(sys.argv)
+    sys.exit(exit_status)
